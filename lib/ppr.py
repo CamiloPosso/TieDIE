8b7e4bac358566bbb50552b2db78e06899b93d2d
CamiloPosso <camilo.posso@pnnl.gov>
2022-12-21 15:09:03 -0800

    Added single diffusion and edge weights to TieDie
    Added single diffusion option to TieDie, which ignores the down_heats and proceeds with only the up_heats.
    
    When using pagerank diffusion only, we also added new feature to set edge weights in the network according to the supplied edge scores. Edge scores are positive, in our application they are -log10 of p-values from an experiment, and they control the edge weights via the lower_w, upper_w. and max_penalty flags. Edge weights are between 0 and 1, and are equal to 1 by default, but can be lowered all the way to zero by using max_penalty = 1, with max_penalty = 0 resulting in no penalty. Edge score below lower_w are applied the max_penalty, while scores above upper_w receive no penalty. In this way, we can easily use the Makefile to get different flow conditions in the network.
    
    Finally, set permutations to optional to save on computation time, and added the saved_diffusion flag to save the diffused heats to the working folder to make it easy to test different size and alpha paramaters. Set to to False by default.

--- lib/ppr.py
+++ lib/ppr.py
@@ -2,10 +2,11 @@ from __future__ import print_function
 from __future__ import division
 #from __future__ import unicode_literals
 import networkx as nx
+import pandas as pd
 
 class PPrDiffuser:
 
-	def __init__(self, network):
+	def __init__(self, network, lower_w, upper_w, max_penalty, edge_weights = None, edge_stress = None, edge_boost = None):
 		'''
 			PPrDiffuser: object to perform the Personalized PageRank Algorithm
 			This method creates the diffuser object from an networkx DiGraph() object,
@@ -19,11 +20,54 @@ class PPrDiffuser:
 		self.G = nx.DiGraph()
 		# create a reversed graph for diffusion from the 'target' set
 		self.G_reversed = nx.DiGraph()
+		self.lower_w = lower_w
+		self.upper_w = upper_w
+		self.max_penalty = max_penalty
+		self.edge_boost = edge_boost
+		self.edge_stress = edge_stress
 		# convert network format to networkX Graph object
 		for source in network:
 			for (i,t) in network[source]:
-				self.G.add_edge(source, t)
-				self.G_reversed.add_edge(t, source)
+				self.G.add_edge(source, t, weight = 1)
+				self.G_reversed.add_edge(t, source, weight = 1)
+
+		if (edge_weights is not None):
+			self.add_edge_weights(edge_weights)
+
+
+	def weight_helper(self, edge_score):
+		## Following a linear function, cutoff input at the given extremes
+		x = max(self.lower_w, edge_score)
+		x = min(self.upper_w, x)
+
+		## This penalizes the weight for low scoring edges
+		## Max weight is 1, smallest is max_penalty. Default weight is also 1
+		## Gives weight 1 edge scores above upper_w.
+		edge_weight = (1 - self.max_penalty) + ((x - self.lower_w)/(self.upper_w - self.lower_w)) * self.max_penalty
+		return edge_weight
+	
+	def add_edge_weights(self, edge_weights):
+		net_w = pd.read_csv(edge_weights, sep = "\t", header = None)
+
+		for i in range(len(net_w.index)):
+			#network[net_w.iloc[i, 1]][net_w.iloc[i, ]]['edge_weigth'] = weight_helper(net_w[4][i])
+			self.G[net_w[0][i]][net_w[2][i]]['weight'] = self.weight_helper(net_w[3][i])
+			self.G_reversed[net_w[2][i]][net_w[0][i]]['weight'] = self.weight_helper(net_w[3][i])
+		
+		if self.edge_stress is not None:
+			net_s = pd.read_csv(self.edge_stress, sep = "\t", header = None)
+			# net_s[3] = net_s[3]**2
+			max_count = max(net_s[3].to_list())
+			print(max_count)
+			if self.edge_boost is None:
+				self.edge_boost = (1 - self.max_penalty)**(-1/2)
+			for i in range(len(net_s.index)):
+				self.G[net_s[0][i]][net_s[2][i]]['weight'] = min(self.G[net_s[0][i]][net_s[2][i]]['weight'] * (net_s[3][i]/max_count) * self.edge_boost, 1)
+				self.G_reversed[net_s[2][i]][net_s[0][i]]['weight'] = min(self.G_reversed[net_s[2][i]][net_s[0][i]]['weight'] * (net_s[3][i]/max_count) * self.edge_boost, 1)
+
+		sanity_check = nx.to_pandas_edgelist(self.G)
+		sanity_check.to_csv('edge_weights_sanity_check.txt', sep = "\t", index = False)
+
 
 	def personal_page_rank(self, p_vector, reverse=False):
 		'''
